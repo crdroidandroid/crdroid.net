f63ab72829b965a245693275ad7e84d34eb6e3b8
Lup Gabriel <gwolf2u.website@gmail.com>
2018-04-24 21:18:33 +0300

    improve error reporting

--- get-devices.php
+++ get-devices.php
@@ -3,83 +3,94 @@
 $branch="8.1";
 $GitHub_devices="https://raw.githubusercontent.com/crdroidandroid/android_vendor_crDroidOTA/" . $branch . "/update.xml";
 
+function GetXMLfromGitHub($url) {
+	$fp = fopen('data', 'w+');
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_FILE, $fp);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+	curl_exec($ch);
+	$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	if($statusCode == 200){
+		rename('data','update.xml');
+	}else{
+		unlink('data');
+		echo "<span style='margin: 0 auto; font-weight: bold; text-align: center;'>Can not read data from GitHub. This is most likely a temporary issue.</span>";
+	}
+}
+
 // Don't spam GitHub with request - it blocks them after a while (temporary ban on domain)
 // Do update every 30 minutes (1800 seconds)
 if (! file_exists('update.xml')) {
-    file_put_contents("data", fopen($GitHub_devices, 'r'));
+    GetXMLfromGitHub($GitHub_devices);
 }else{
     if (filemtime('update.xml') + 1800 < strtotime('now')) {
-        file_put_contents("data", fopen($GitHub_devices, 'r'));
-        
-        if (filesize('data') > 0) {
-            rename('data','update.xml');
-        }else{
-            error_reporting(E_ERROR);
-            echo "<span style='margin: 0 auto; font-weight: bold; text-align: center;'>Can not read data from GitHub. This is most likely a temporary issue.</span>";
-        }
-    }
+		GetXMLfromGitHub($GitHub_devices);
+   }
 }
 
-$xml = simplexml_load_file('update.xml');
-foreach($xml as $manufacturer){
-    //manufacturer
-    echo "
+if (file_exists('update.xml')) {
+	$xml = simplexml_load_file('update.xml');
+	foreach($xml as $manufacturer){
+		//manufacturer
+		echo "
     <div class='manufacturer'>
         <span class='ti-folder'> " . $manufacturer['id'] . "</span>
     </div>";
 
-    //devices
-    foreach ($manufacturer as $k => $v) {
-        $build_date = explode("-", $v->filename);
-        $maintainer_arr = $v->maintainer;
-        $maintainer = explode("(", $maintainer_arr);
-        if (isset($maintainer[1])) {
-            $nick = str_replace(")","",$maintainer[1]);
-        }else{
-            $nick = null;
-        }
-    echo "
+		//devices
+		foreach ($manufacturer as $k => $v) {
+			$build_date = explode("-", $v->filename);
+			$maintainer_arr = $v->maintainer;
+			$maintainer = explode("(", $maintainer_arr);
+			if (isset($maintainer[1])) {
+				$nick = str_replace(")","",$maintainer[1]);
+			}else{
+				$nick = null;
+			}
+		echo "
         <div class='device'>
             <div class='main'>
             <span class='ti-mobile'> Device name: " . $v->devicename . "</span><br>
             <span class='ti-receipt links'> Device codename: <a href='https://crdroid.net/" . $k . "' rel='bookmark'>" . $k . "</a></span></br>
             <span class='ti-user'> Maintainer: " . $maintainer[0] . "</span><br>";
-        if (!empty($nick)) {
-            echo "
+			if (!empty($nick)) {
+				echo "
             <span class='ti-id-badge'> Nickname: " . $nick . "<span><br>";
-        }
-    echo "
+			}
+		echo "
             <span class='ti-android'> crDroid version: " . $build_date[4] . "</span><br>
             <span class='ti-calendar'> Last build: " . $build_date[2] . "</span><br>
             <span class='ti-pencil-alt'> Build type: " . $v->buildtype . "<span><br>
         </div>
         <div class='dl'>";
-        if (empty($v->download)) {
-            echo "
+			if (empty($v->download)) {
+				echo "
             <a href='#downloads' class='btn btn-disabled' title='Unavailable'><span class='ti-face-sad'></span> Download crDroid</a>";
-        }else{
-            echo "
+			}else{
+				echo "
             <a href='" . $v->download . "' class='btn btn-dark' target='_blank' title='" . $v->filename . "'><span class='ti-import'></span> Download crDroid</a>";
-        }
-        if (empty($v->gapps)) {
-            echo "
+			}
+			if (empty($v->gapps)) {
+				echo "
             <a href='#downloads' class='btn btn-disabled' title='Unavailable'><span class='ti-face-sad'></span> Google Apps</a>";
-        }else{
-            echo "
+			}else{
+				echo "
             <a href='" . $v->gapps . "' class='btn btn-orange' target='_blank'><span class='ti-package'></span> Google Apps</a>";
-        }
-        if (empty($v->forum)) {
-            echo "
+			}
+			if (empty($v->forum)) {
+				echo "
             <a href='#downloads' class='btn btn-disabled' title='Unavailable'><span class='ti-face-sad'></span> Support Forum</a>";
-        }else{
-            echo "
+			}else{
+				echo "
             <a href='" . $v->forum . "' class='btn btn-light' target='_blank'><span class='ti-comments-smiley'></span> Support Forum</a>";
-        }
-    echo "
+			}
+		echo "
             </div>
         </div>
         ";
-    }
+		}
+	}
+	echo "<div class='timestamp'>Showing devices from cache set " . date("F d Y H:i:s.", filemtime('update.xml')). "</div>";
 }
-echo "<div class='timestamp'>Showing devices from cache set " . date("F d Y H:i:s.", filemtime('update.xml')). "</div>";
 ?>
