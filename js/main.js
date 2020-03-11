292be0672c38e1129435fce28dbebf4d4bddac90
Lup Gabriel <gwolf2u.website@gmail.com>
2020-03-11 20:19:28 +0200

    re-add carousel

--- js/main.js
+++ js/main.js
@@ -109,6 +109,25 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
+  // Init Owl Carousel
+  $('.owl-carousel').owlCarousel({
+    items: 4,
+    autoplay: true,
+    loop: true,
+    margin: 30,
+    dots: true,
+    responsiveClass: true,
+    responsive: {
+
+      320: { items: 1},
+      480: { items: 2},
+      600: { items: 2},
+      767: { items: 3},
+      768: { items: 3},
+      992: { items: 4}
+    }
+  });
+
 // custom code
 
 });
