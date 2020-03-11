1c5c43ca20f43b34779d079a072c66e7d2e80e31
Lup Gabriel <gwolf2u.website@gmail.com>
2020-03-11 20:19:28 +0200

    remove unused js code

--- js/main.js
+++ js/main.js
@@ -109,28 +109,6 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
-  // Modal video
-  new ModalVideo('.js-modal-btn', {channel: 'youtube'});
-
-  // Init Owl Carousel
-  $('.owl-carousel').owlCarousel({
-    items: 4,
-    autoplay: true,
-    loop: true,
-    margin: 30,
-    dots: true,
-    responsiveClass: true,
-    responsive: {
-
-      320: { items: 1},
-      480: { items: 2},
-      600: { items: 2},
-      767: { items: 3},
-      768: { items: 3},
-      992: { items: 4}
-    }
-  });
-
 // custom code
 
 });
