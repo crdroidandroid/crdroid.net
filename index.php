d6a85ad1036f8bf88d008f7adea8e4b7dcb879fc
Lup Gabriel <gwolf2u.website@gmail.com>
2022-12-17 20:05:47 +0200

    redirect www to non-www
    just help on analytics

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+  if (substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.') {
+    header('Location: http'.(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on' ? 's':'').'://' . substr($_SERVER['HTTP_HOST'], 4).$_SERVER['REQUEST_URI']);
+    exit;
+  }
+
   include 'functions.php';
   $domain = GetDomain();
 ?>
