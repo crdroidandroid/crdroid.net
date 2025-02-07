2b36570533d170f0aeb6611339070634e0b6961f
Lup Gabriel <gwolf2u.website@gmail.com>
2025-02-07 21:27:00 +0200

    put back old paypal url

--- donate.php
+++ donate.php
@@ -160,8 +160,8 @@
 
         <div class="col-md col-lg">
           <div class="feature-block">
-		  	<button type="button" class="btn btn-primary btn-lg m-3" onclick="window.open('https://paypal.me/gwolf2u')"><i class='bx bxl-paypal' ></i> Donate on Paypal</button>
-			<button type="button" class="btn btn-warning btn-lg m-3" onclick="window.open('http://patreon.com/join/crdroidandroid')"><i class='bx bxl-patreon' ></i> Become a Patreon</button>
+          <button type="button" class="btn btn-primary btn-lg m-3" onclick="window.open('https://paypal.me/crdroidandroid')"><i class='bx bxl-paypal' ></i> Donate on Paypal</button>
+			    <button type="button" class="btn btn-warning btn-lg m-3" onclick="window.open('https://patreon.com/join/crdroidandroid')"><i class='bx bxl-patreon' ></i> Become a Patreon</button>
           </div>
         </div>
 
